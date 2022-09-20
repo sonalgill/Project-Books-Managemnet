fca067c502172bbc72dca3f13169a3ec7867f4e5
unknown <shankarkhade9@gmail.com>
2022-09-20 13:11:03 +0530

    bookschema

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,18 +3,34 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema(
     {
-        title: {type:String, required:true, unique:true},
-        excerpt: {type:String, required:true}, 
+        title: {type:String,
+             required:true, 
+             unique:true },
+
+        excerpt: {type:String, 
+            required:true},
+
         userId: {  type: ObjectId,
             required: true,
             ref: "user"},
-        ISBN: {type:String, required:true, unique:true},
-        category: {type:String, required:true},
-        subcategory:{ type:[String],required:[true]},
-        reviews: {type:Number, default: 0},
+
+        ISBN: {type:String,
+             required:true,
+              unique:true },
+
+        category: {type:String,
+             required:true },
+
+        subcategory:{ type:[String],
+            required:[true] },
+
+        reviews: {type:Number, 
+            default: 0 },
         deletedAt: {type:Date}, 
-        isDeleted: {type:boolean, default: false},
-        releasedAt: {type:Date, required:true, }   
+        isDeleted: {type:boolean,
+             default: false },
+        releasedAt: {type:Date,
+             required:true, }   
         
       },  {timestamps:true})
      
