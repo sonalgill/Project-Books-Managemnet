0553ed2f7577bb9c7dc84a5d94dd28fc72324d90
Gouravpundir <pundirgourav0@gmail.com>
2022-09-22 20:15:15 +0530

    updated

--- src/controllers/bookcontroller.js
+++ src/controllers/bookcontroller.js
@@ -76,7 +76,10 @@ const getBooks = async function (req, res) {
   }
 };
 
-//-- get book by bookID
+
+
+
+//=============================get book by bookID===============================//
 let getBookById = async (req, res) => {
   try {
     let bookId = req.params.bookId;
@@ -113,4 +116,50 @@ let getBookById = async (req, res) => {
   }
 };
 
-module.exports = { createBook, getBooks, getBookById };
+//============================update book========================================//
+
+const updateBook=async(req,res)=>{
+  try{
+  let requestBody=req.body
+  if(!isvalidRequestBody(requestBody)){
+    res.status(400).send({statua:false, msg:`Request body can't be empty`})
+    return
+  }
+  let bookId=req.params.bookId
+  if (!isvalidObjectId(bookId)) {
+    res
+      .status(400)
+      .send({ status: false, message: `${bookId} is not a valid book id ` });
+      return
+  }
+
+  let uniqueTitleAndIsbn = await bookModel.find({ $or: [{ title: requestBody.title }, { ISBN: requestBody.ISBN }] });
+  if (uniqueTitleAndIsbn.length !== 0) {
+      if (uniqueTitleAndIsbn [0].title == requestBody.title) return res.status(400).send({ status: false, data: "Title is Already Exists,Please Input Another title" })
+      else { return res.status(400).send({ status: false, data: "ISBN Number Already Exists,Please Input Another ISBN Number" }) }
+  };
+
+  const book = await bookModel.findOne({_id: bookId});
+    if (book.isDeleted === false) {
+      let updatedBookData = await bookModel.findByIdAndUpdate(
+        bookId,
+        {
+          title: requestBody.title,
+          excerpt: requestBody.excerpt,
+          releasedAt: requestBody.releasedAt,
+          ISBN: requestBody.ISBN
+        },
+        { new: true }
+      );
+      res.status(200).send({ status: true, data: updatedBookData });
+    } else {
+      res.status(404).send("File is not present or Deleted");
+    }
+}catch(err){
+  res.status(500).send({status:false, error:err.message})
+}
+}
+
+
+
+module.exports = { createBook, getBooks, getBookById, updateBook };
